a1e4433390d4829fd2331384f2fb7cbf5b908a86
MisterGPS <mistergpsnew@gmail.com>
2022-07-18 15:16:09 +0200

    Puzzle update

--- Assets/Scenes/Room1/Assets/Puzzles/BabyPuzzle/Puzzle.cs
+++ Assets/Scenes/Room1/Assets/Puzzles/BabyPuzzle/Puzzle.cs
@@ -1,22 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
 using UnityEngine;
 
 public class Puzzle : MonoBehaviour
 {
     public Material puzzleMaterial;
-    private List<PuzzlePiece> puzzlePieces;
+    private List<PuzzlePiece> puzzlePieces = new List<PuzzlePiece>();
+    private List<BoxCollider> puzzleTargets = new List<BoxCollider>();
     
     [SerializeField]
     private Vector2Int numTiles;
 
-    private Vector2 scale = new Vector2(1, 1);
-
+    private Vector2 tileScale;
+    private Vector2 distributionScale = new Vector2(1.0f, 1.0f);
+    
     private void Start()
     {
         SpawnTiles();
+        DistributeTiles();
     }
 
     private void SpawnTiles()
@@ -36,18 +38,47 @@ public class Puzzle : MonoBehaviour
                 PuzzlePiece puzzle = newPiece.AddComponent<PuzzlePiece>();
                 puzzle.Position = new Vector2Int(i, j);
                 puzzle.PuzzleMaterial = puzzleMaterial;
-                puzzle.Instantiate(numTiles, scale);
+                puzzle.Instantiate(numTiles);
+                puzzlePieces.Add(puzzle);
+
+                GameObject puzzleTarget = new GameObject();
+                BoxCollider targetCollider = puzzleTarget.AddComponent<BoxCollider>();
             }
         }
+        UnityEngine.Debug.Assert(puzzlePieces.Count > 0 && puzzlePieces[0] != null);
+        tileScale = puzzlePieces[0].GetComponent<BoxCollider>().size;
     }
 
     private void DistributeTiles()
     {
-        // for (int i = 0; i < )
+        float startX = tileScale.x * distributionScale.x / 2.0f;
+        float startY = tileScale.y * distributionScale.y / 2.0f;
+        float width = tileScale.x * distributionScale.x / numTiles.x;
+        float height = tileScale.y * distributionScale.y / numTiles.y;
+
+        // Swap position of tiles
+        for (int i = 0; i < numTiles.x; i++)
+        {
+            for (int j = 0; j < numTiles.y; j++)
+            {
+                
+            }
+        }
+        
+        // Offset tiles
+        foreach (PuzzlePiece puzzlePiece in puzzlePieces)
+        {
+            
+        }
     }
 
     private void Solved()
     {
         
+    }
+    
+    private void OnMouseOver()
+    {
+        print("Over objects");
     }
 }
